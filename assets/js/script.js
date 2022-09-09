ac9629c7be8827a32ce979da93790b050a0241d4
Emily Bass <55358818+emilydbass@users.noreply.github.com>
2022-09-09 14:34:54 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".workSlider", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.work-button-next',
+    },
+});
